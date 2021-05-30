dfd01636d3df252e5124d2a610d929ce409317ce
Mason Anders <mason@rideos.ai>
2021-05-30 13:43:24 -0700

    Structure and style Timer

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -1,5 +1,21 @@
 import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: grid;
+  grid-template-columns: 0 2;
+  font-size: 128px;
+  text-align: center;
+  h4 {
+    font-size: 24px;
+  }
+`;
 
 export default function ({ children }) {
-  return <div>{children}</div>;
+  return (
+    <Container>
+      {children}
+      <h4>Elapsed time</h4>
+    </Container>
+  );
 }
