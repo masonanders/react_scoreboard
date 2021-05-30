c4c6fbc33184316c202369da910867a075750686
Mason Anders <mason@rideos.ai>
2021-05-30 13:46:19 -0700

    Remove unused styles in Timer

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -2,8 +2,6 @@ import React from "react";
 import styled from "styled-components";
 
 const Container = styled.div`
-  display: grid;
-  grid-template-columns: 0 2;
   font-size: 128px;
   text-align: center;
   h4 {
